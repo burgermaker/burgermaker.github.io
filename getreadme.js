1bc5ac9f5f6d121c87b8cd89f05071cb82104830
burgermaker <39492889+burgermaker@users.noreply.github.com>
2018-05-22 08:36:25 -0700

    Ok fr this time it actually works :sparkles:

--- getreadme.js
+++ getreadme.js
@@ -1,27 +1,18 @@
 var xhr = new XMLHttpRequest();
 
-function processRequest(e) {
-//     if (xhr.readyState == 4 && xhr.status == 200) {
-//         console.log(xhr.responseText);
-//     }
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 xhr.open('GET', "https://raw.githubusercontent.com/burgermaker/burgermaker.github.io/master/README.md", true);
+
+function processRequest(e) {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+      var readget = xhr.responseText.toString();
+      var div = document.getElementById('main');
+      var node = document.createElement("P");
+      var textnode = document.createTextNode(readget);
+      node.appendChild(textnode);
+      document.getElementById("main").append(node);
+  }
+}
+
 xhr.addEventListener("readystatechange", processRequest, false);
+
 xhr.send();
-
-await sleep(500);
-
-var readget = xhr.responseText.toString();
-
-//process readget with markup
-
-var div = document.getElementById('main');
-var node = document.createElement("P");
-var textnode = document.createTextNode(readget);
-node.appendChild(textnode);
-document.getElementById("main").append(node);
