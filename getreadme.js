c6e42809ff3f8c72f848d3111be8c5b9b596094e
King Kale <33632547+why-does-ie-still-exist@users.noreply.github.com>
2018-05-21 21:28:53 -0700

    Yay it works :sparkles:
    _Paaaaarrrrtay_ :cake:

--- getreadme.js
+++ getreadme.js
@@ -1,17 +1,27 @@
 var xhr = new XMLHttpRequest();
 
 function processRequest(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        console.log(xhr.responseText);
-    }
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         console.log(xhr.responseText);
+//     }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 xhr.open('GET', "https://raw.githubusercontent.com/burgermaker/burgermaker.github.io/master/README.md", true);
 xhr.addEventListener("readystatechange", processRequest, false);
 xhr.send();
 
+await sleep(500);
+
+var readget = xhr.responseText.toString();
+
+//process readget with markup
+
 var div = document.getElementById('main');
 var node = document.createElement("P");
-var textnode = document.createTextNode(xhr.responseText.toString());
+var textnode = document.createTextNode(readget);
 node.appendChild(textnode);
 document.getElementById("main").append(node);
