2e9f3ca28e71d944e5ec759780ca906365580a26
King Kale <33632547+why-does-ie-still-exist@users.noreply.github.com>
2018-05-22 11:53:09 -0700

    Update getreadme.js

--- getreadme.js
+++ getreadme.js
@@ -4,7 +4,10 @@ xhr.open('GET', "https://raw.githubusercontent.com/burgermaker/burgermaker.githu
 function mdreplace(str){
   document.getElementById(str).innerHTML = micromarkdown.parse(
   document.getElementById(str).innerHTML.toString());
+  document.getElementById("main").outerHTML = document.getElementById("main").outerHTML.replace("<img","<!--").toString();
+  document.getElementById("main").outerHTML = document.getElementById("main").outerHTML.replace("</img>","-->").toString();
 }
+
 function processRequest(e) {
   if (xhr.readyState == 4 && xhr.status == 200) {
       document.getElementById("main").innerHTML = "";
