7de867b1c2d0270e8a2c3413da8ad5206cb51aff
burgermaker <39492889+burgermaker@users.noreply.github.com>
2018-05-22 14:43:18 -0700

    Update getreadme.js

--- getreadme.js
+++ getreadme.js
@@ -17,9 +17,9 @@ function processRequest(e) {
   if (xhr.readyState == 4 && xhr.status == 200) {
       document.getElementById("main").innerHTML = "";
       var readget = xhr.responseText.toString();
-      readget = readget.replace("# ","#");
+      readget = readget.replace("#.","");
       var div = document.getElementById('main');
-      var node = document.createElement("");
+      var node = document.createElement("h1");
       var textnode = document.createTextNode(readget);
       node.appendChild(textnode);
       document.getElementById("main").append(node);
