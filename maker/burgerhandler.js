9f46a456fec2afd0bd0d72bf696e14a83120d645
King Kale <33632547+why-does-ie-still-exist@users.noreply.github.com>
2018-05-27 13:57:35 -0700

    Add files via upload
    Add highlight button

--- maker/burgerhandler.js
+++ maker/burgerhandler.js
@@ -1,3 +1,4 @@
+var htoggled = false;
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -16,6 +17,21 @@ function addNewDiv(iter){
   for(var i=1; i <= iter; i++){
     document.getElementsByClassName("builder")[0].innerHTML += "<div id=IngredientDiv class=\"dropzone js-drop\"></div>";
   }
+}
+
+function highlight(){
+	var arr = [].slice.call(document.querySelectorAll("#ingredientlist")[0].children);
+	if(!htoggled){
+	arr.forEach(function(element) {
+  element.style.filter = "hue-rotate(180deg)"
+});
+	htoggled = true;
+	} else {
+	arr.forEach(function(element) {
+  element.style.filter = "hue-rotate(0deg)"
+});
+	htoggled = false;
+	}
 }
 
 addNewDiv(3);
