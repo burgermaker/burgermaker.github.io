6875479fb96e334f99bbf3625fc026819dfd5245
King Kale <33632547+why-does-ie-still-exist@users.noreply.github.com>
2018-06-04 08:04:27 -0700

    Update howler_implement.js

--- maker/howler_implement.js
+++ maker/howler_implement.js
@@ -1,1 +1,12 @@
+var sound = new Howl({
+  src: ['Boofs Burgers.wav'],
+  autoplay: true,
+  loop: true,
+  volume: 1.0,
+  onend: function() {
+    console.log('Finished!');
+  }
+});
 
+sound.play();
+console.log("your mom gay");
