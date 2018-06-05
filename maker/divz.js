962dd4e14290058ae1304a40a620aaaf7d42bcd4
King Kale <33632547+why-does-ie-still-exist@users.noreply.github.com>
2018-06-04 17:52:28 -0700

    Move elements up a little bit

--- maker/divz.js
+++ maker/divz.js
@@ -3,6 +3,6 @@ function updatedivz(){
 	var n;
 	for(i = 0; i < x.length; i++){
 	n = 99-i;
-	x[i].setAttribute("style","z-index: " + n + ";");
+	x[i].setAttribute("style","z-index: " + n + "; margin-top: -25px;");
 	}
 }
