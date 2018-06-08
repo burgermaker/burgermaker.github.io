9438456bdfaaf45c0eee1ba20cae169116f6ad31
supergroverr <39493191+supergroverr@users.noreply.github.com>
2018-06-08 15:26:00 +0000

    try to move boxes

--- maker/divz.js
+++ maker/divz.js
@@ -3,7 +3,7 @@ function updatedivz() {
 	var n;
 	for (i = 0; i < x.length; i++) {
 		n = 99 - i;
-		x[i].setAttribute("style", "z-index: " + n + "; margin-top: -25px;");
+		x[i].setAttribute("style", "z-index: " + n + "; margin-top: -40px;");
 	}
 }
 
